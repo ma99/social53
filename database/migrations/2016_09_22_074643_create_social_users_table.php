b36a474e784d9a4e62c9a893e6836ba2d516492d
mas <ma99.dev@gmail.com>
2016-09-22 10:57:52 +0300

    Add Relationship with User

---
+++ database/migrations/2016_09_22_074643_create_social_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSocialUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('social_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->string('provider_user_id')->unsigned();
+            $table->string('provider');
+            $table->string('avatar');
+            $table->timestamps();
+           
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('social_users');
+    }
+}
